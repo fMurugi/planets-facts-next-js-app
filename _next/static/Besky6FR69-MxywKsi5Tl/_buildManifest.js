1cb0a18ae3e9d4bc4029e81a332b9f3ec4feebe0
fMurugi <81636349+fMurugi@users.noreply.github.com>
2022-08-31 14:22:20 +0000

    Deploying to gh-pages from @ fMurugi/planets-facts-next-js-app@32f499f2cfad7efa542e7dd556298c74e2ed7085 🚀

--- _next/static/Besky6FR69-MxywKsi5Tl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/336-38720de845247c7b.js","static/css/6c24f0f32dd3e708.css","static/chunks/pages/index-d7469a5dd83b7719.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/gsY9h3H22igIsNFq3J-rn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/336-379e947c90fc2905.js","static/css/6c24f0f32dd3e708.css","static/chunks/pages/index-22775556b9e98ea6.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
